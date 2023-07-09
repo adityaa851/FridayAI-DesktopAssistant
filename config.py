69f2fcfb2c6de788b5f6ef4e132fedfd571dc10a
adityaa851 <102279064+adityaa851@users.noreply.github.com>
2023-07-09 15:45:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-apikey = "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"
-apikey_weather = "fb51a3cd07d675cec99d050aa35f7147"
-apikey_news = "a8c0649cddf14be3817cad0cd1544c9f"
+apikey = "open ai api key"
+apikey_weather = "openweathermap api key"
+apikey_news = "news api key"
